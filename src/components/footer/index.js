6f80c1369f8b1e2a5e9600760b720d93dd9312f6
Ramom101001 <97475246+Ramom101001@users.noreply.github.com>
2023-08-28 21:34:28 -0300

    quarto commit

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,39 +1,55 @@
 import React from 'react';
 import './footer.css';
 
-import img2 from "../../../public/eu.png";
+import logo from "../../assets/logo.png"
+
+import wpp from "../../assets/whatsapp.png"
+import git from "../../assets/github.png"
+import linkd from "../../assets/linkedin.png"
 
 function Footer() {
     return (
         <footer className="footer">
-            <div id='footer-new' className='footer-new'>
                 <div className="container">
-                    <div className='footer-new__holder position-relative'>
+                    <div className='footer__line'>
 
-                        <div className="name-logo">  
-                            <img src={img2} className='logo' alt='logo'/>
-                            <p>RAMOM</p>
+                        <div className="footer__logo">  
+                        
+                            <div className='footer__logo__name'>
+                                <img src= {logo} alt='imagem logo'/>
+                                <p>RAMOM</p>
+                            </div>
+
+                            <div className='footer__logo__about'>
+                                <p>eu sou pica eacnajkaneCb nkjhgvz bnkjxhhvj mazjxhkhbm nmckh,
+                                    nxcjbkdn mvck.n xfmcb nscmnjbn mscbnjn mfcmbnkn, cbmn, dmcbnkjnd,
+                                    njn llvg vbnjhvm nnjhvgn nkjhvbn kjhvn bnhgvbbnhg
+                                </p>
+                            </div>
                         </div>
 
-                        <div className="contact-info">
+                        <div className="footer__info">
+                            <p>Dados:</p>
                             <p>Email: ramom101001@gmail.com</p>
                             <p>Telefone: (21) 99788-5085</p>
-                            <p>Redes Sociais: <a href="https://linkedin.com/seulinkedin">LinkedIn</a>, <a href="https://github.com/seugithub">GitHub</a></p>
-                        </div>
-
-                        <div className="copyright-info">
-                            <p>&copy; {new Date().getFullYear()} Seu Nome. Todos os direitos reservados.</p>
-                            <p>Créditos para ícones: <a href="https://www.flaticon.com/">Flaticon</a></p>
-                        </div>
-
-                        <div className="location-info">
                             <p>Localização: Sua Cidade, Seu País</p>
                         </div>
 
-                    </div>
-                </div>
-            </div>
+                        <div className="footer__social">
+                            <img src= {wpp} alt='acae'/>
+                            <img src= {git} alt='aecaec'/>
+                            <img src= {linkd} alt='acecea'/>
 
+                        </div>
+
+                    </div>
+
+                    <div className='footer__line'>
+                        <p>&copy; {new Date().getFullYear()} Seu Nome. Todos os direitos reservados.</p>
+                        <p>Créditos para ícones: <a href="https://www.flaticon.com/">Flaticon</a></p>
+                    </div>
+
+                </div>
         </footer>
     );
 }
